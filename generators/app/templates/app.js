277e2832f30371bd0a8c8117808f1605b29259f1
Anthony Medina <antmedina91@gmail.com>
2019-03-17 15:47:52 +0000

    add basic error handling to templates

--- generators/app/templates/app.js
+++ generators/app/templates/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const apiRouter = require('./routes/api');
+const { routeNotFound, handle500 } = require('./errors');
 
 const app = express();
-
-const apiRouter = require('./routes/api');
 
 app.use(bodyParser.json());
 
 app.use('/api', apiRouter);
+
+app.all('/*', routeNotFound);
+
+app.use(handle500);
 
 module.exports = app;
